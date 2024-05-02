f671e277dbc4a29ba8661a95739d0a356e384ac3
Galvez Lautaro <80927839+Atarox228@users.noreply.github.com>
2024-05-02 16:08:05 -0300

    ProductoTest completado, revisar comportanmiento funcional

--- test/ar/edu/unq/po2/tp5/mercado/ProductoTest.java
+++ test/ar/edu/unq/po2/tp5/mercado/ProductoTest.java
@@ -2,19 +2,50 @@ package ar.edu.unq.po2.tp5.mercado;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import ar.edu.unq.po2.tp5.mercado.Producto;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ProductoTest {
 
-	@Test
-	public void testConstructor() {
-		assertEquals("Arroz", arroz.getNombre());
-		assertEquals(new Double(18.9), arroz.getPrecio());
-		assertTrue(arroz.esPrecioCuidado());
-		
-		assertEquals("Vino", vino.getNombre());
-		assertEquals(new Double(55), vino.getPrecio());
-		assertFalse(vino.esPrecioCuidado());
-	}
+  private Producto arroz;
+  private Producto detergente;
 
+  @BeforeEach
+  public void setUp() throws Exception {
+
+    arroz = new ProductoCooperativa("Arroz", 18.9d , 10 );
+    leche = new ProductoCooperativa("Leche", 22.5d , 5 , 30d);
+    detergente = new ProductoEmpresaTradicional("Detergente", 50d, 1);
+
+  }
+  
+  @Test
+  public void testConstructor() {
+    assertEquals("Arroz", arroz.getName());
+    assertEquals(18.0d, arroz.getPrice(),0.001);
+    assertEquals(10, arroz.getStock());
+
+    assertEquals("Leche", leche.getName());
+    assertEquals(15.75d, leche.getPrice(),0.001);
+    assertEquals(5, leche.getStock());
+
+    assertEquals("Detergente", detergente.getName());
+    assertEquals(50d, detergente.getPrice());
+    assertEquals(1, detergente.getStock());
+  }
+
+
+  @Test 
+  public void testReducirStack(){
+    arroz.reducirStock();
+    assertEquals(9, arroz.getStock());
+    
+    detergente.aumentarStock();
+    assertEquals(2, detergente.getStock());
+  }
+
+
+  
 }
